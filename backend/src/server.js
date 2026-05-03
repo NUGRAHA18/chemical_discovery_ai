8114978f6478523bd1d41b168bc6dfb03a119426
Agung Developer <Agung Nugraha>
2026-05-04 00:25:59 +0700

    fix configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -110,7 +110,7 @@ app.use((err, req, res, next) => {
 
 initializeSocket(server);
 
-const PORT = process.env.PORT || 3010;
+const PORT = process.env.PORT || 3010 || '0.0.0.0';
 server.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
   console.log(`✅ Socket.io ready`);
